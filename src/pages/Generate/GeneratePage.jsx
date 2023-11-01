cac6ac615de0838cedf31a8f1eafbbf6c3e56f2b
zsidoruben <zsruben03@gmail.com>
2023-11-01 18:47:53 +0100

    Removed unneccessary code

--- src/pages/Generate/GeneratePage.jsx
+++ src/pages/Generate/GeneratePage.jsx
@@ -23,48 +23,6 @@ const GeneratePage = () => {
   const [activeStep, setActiveStep] = useState(0);
   const currentValidation = validations[activeStep];
 
-  const getWord = (array, beginning, end) => {
-    return array.slice(beginning, end).join("");
-  };
-  const getEndIndexes = (charArray) => {
-    const endIndexes = [];
-    for (let i = 0; i < charArray.length; i++) {
-      const char = charArray[i];
-      if (i + 1 === charArray.length) {
-        break;
-      }
-      const nextChar = charArray[i + 1];
-      if (nextChar === char) {
-        endIndexes.push(i + 1);
-      }
-    }
-    return endIndexes;
-  };
-  const LNRCS = (str) => {
-    console.clear();
-    const charArray = str.split("");
-    const endIndexes = getEndIndexes(charArray);
-    const words = [];
-    let prevEnd = 0;
-    for (let i = 0; i < endIndexes.length; i++) {
-      const endindex = endIndexes[i];
-      words.push(getWord(charArray, prevEnd, endindex));
-      prevEnd = endindex;
-    }
-    words.push(
-      getWord(
-        charArray,
-        endIndexes[endIndexes.length - 1],
-        charArray[charArray.length]
-      )
-    );
-    console.log(words);
-    console.log('longest:' + words.);
-    
-  };
-
-  LNRCS("ABCDDEFGHHIIJKLMNNOPQRSSSTUV");
-
   const _renderStepContent = (step) => {
     switch (step) {
       case 0:
